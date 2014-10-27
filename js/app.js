3a02b6defc79b9494cd972e3268239c832d75050
Lukasus <angelluk@ukr.net>
2014-10-27 13:11:22 +0200

    final commit

--- js/app.js
+++ js/app.js
@@ -1,16 +1,77 @@
  var  DataTable; 
 
- function BuildData(){
+function AddEvent(){		// переназначаем обработчики после обновления таблицы
 
- 	var myHtml  = "<table  id=\"Employees\"class=\" table table-bordered\">";
+ 	$( "#Employees th").on( "click", function( eventObject ) {
+ 	   
+ 	   var elem = $(this);
+
+ 	   if (elem.text()!="Manager" && elem.text()!="Certificate"){
+
+ 	   	 DataTable.SearchField = elem.text();
+
+ 	   	 console.log("Clicking on " + elem.text());
+
+ 	   	 if(elem.hasClass("Up")){DataTable.OrderSortUp=false};		// переключение сортировки
+ 	   	 if(elem.hasClass("Down")){DataTable.OrderSortUp=true};
+
+ 	   	 SortData(DataTable.Data.employees,DataTable.SearchField,DataTable.OrderSortUp); 
+ 	   	  
+ 	    //$("#Employees").html(" ");
+ 	   	
+ 	   	 BuildData(DataTable.Data.employees); 
+ 	    }
+
+ 	});
+ }	
+//---------------------------------------- Сортировка данных ----------------------------
+ function SortData(arr,sortstr,order){
+ 		
+ 		if (sortstr=="FirstName" || sortstr=="LastName"  || sortstr=="Department" ){ //сортировка по алфавиту
+ 			arr.sort(function(a,b){
+ 			var fielda=a[sortstr].toLowerCase()
+ 			var fieldb=b[sortstr].toLowerCase()
+ 			if (order==true)
+ 				{ return (fielda<fieldb)? -1 : (fielda>fieldb)? 1 : 0 }
+ 			else
+ 				{ return (fielda<fieldb)? 1 : (fielda>fieldb)? -1 : 0 }
+ 			})
+ 		}
+ 		if(sortstr=="Rate" || sortstr=="Salary" ){				// сортировка по числам
+			
+ 			arr.sort(function(a,b){
+ 			var fielda=(+a[sortstr])
+ 			var fieldb=(+b[sortstr])
+ 			if (order==true)
+ 				{ return (fielda>fieldb)? 1 : (fielda<fieldb)? -1 : 0 }
+ 			else
+ 				{ return (fielda>fieldb)? -1 : (fielda<fieldb)? 1 : 0 }
+ 		   })
+  		}
+}
+ 
+//----------------------------------------Создание таблицы ----------------------------
+ function BuildData(arr){
+
+ 	var myHtml;
  	
- 	myArray = DataTable.Data.employees;
+ 	myArray = arr;
 
  	// ----------------- Создаем динамически струткутру таблицы------------------
  	 myHtml += "<tr>"; 
 
  	 for (var header in myArray[0]) {				// заполняем динамически струткутру сотрудника
-	 	      myHtml += "<th>" +  header  + "</th>";	
+	 	     
+	 	     if(header == DataTable.SearchField){
+	 	     	if(DataTable.OrderSortUp==true){
+	 	     		myHtml += "<th class=\"Up\">" +  header  + "</th>";
+	 	     	}
+	 	     	else if (DataTable.OrderSortUp==false){
+	 	     		myHtml += "<th class=\"Down\">" +  header  + "</th>";
+	 	     	}
+	 	     }
+	 	     else
+	 	      { myHtml += "<th>" +  header  + "</th>";}	
 	 	   }
 
 	 myHtml +=  "</tr>"; 	   
@@ -23,13 +84,16 @@
 		   for (var prop in item) {			// заполняем свойствами сотрудника
 
 		   	switch (prop) {
+
 		   	   case "Salary":
 		   	      myHtml += "<td>" + "$" +Math.floor(item[prop])  + "</td>"; 		// зарплата без центов
 		   	      break;
+
 		   	   case "Manager":
 		   	   case "Certificate":
 		   	      myHtml += "<td>" + (item[prop]===true? "да" : "нет") + "</td>";	// удобочитаемые логические  значения
 		   	      break;
+
 		   	   case "Rate":
 		   	      {
 		   	      	if (parseFloat(item[prop])>0) {
@@ -42,6 +106,7 @@
 		   	      		myHtml += "<td>" +  parseFloat(item[prop])  + "</td>";
 		   	      };
 		   	      break; 
+
 		   	   default:
 		   	      myHtml += "<td>" +  item[prop]  + "</td>";	
 		   	}
@@ -52,33 +117,39 @@
  	 
  	});
  	
- myHtml +=  "</table>"; 
+
+ //$("#Employees").html(" ");
 
  $("#Employees").html( myHtml ); // прорисовываем таблицу 
 
+ AddEvent();
+
  } 
 
+ //------------------------------ Main --------------------------------------------
  $(document).ready(function(){
 
- 	DataTable = {
- 		SearchField:"Firstname",
+ 	DataTable = {					// создаем модель таблицы
+ 		SearchField:"FirstName",
  		OrderSortUp:true,
  		Data: []
  	};
 
- 	$.ajax({		// read data from JSON 
+ 	$.ajax({		// читаем с  JSON 
  	      
  	      type: "GET",
  	      url: "data/Empls.json",
  	      dataType: "json",
  	      async:false,
- 	      cache:false,
+ 	      cache:true,
  	      
  	      success: function( json ) {
 
  	      	DataTable.Data = json;
+
+ 	      	SortData(DataTable.Data.employees,DataTable.SearchField,DataTable.OrderSortUp) // первоночальная сортировка по  FirstName
  	    
- 	         BuildData(); 		// операции с DOM сведены к минимуму
+ 	        BuildData(DataTable.Data.employees); 		// операции с DOM сведены к минимуму
  	      },
 
  	      error: function( xhr, status, errorThrown ) {
@@ -87,10 +158,9 @@
   	         }
  	  });
 
- 	//$("#Employees td.More").css("background", "#DEDEFE");
- 	//$("#Employees td.Less").css("background", "#FEDAE0");
-
- 	// намного быстрее так CSS биндится для td  чем выше вариант
- 	$( "<style> #Employees td.More { background-color: #DEDEFE } td.Less { background-color: #FEDAE0 } 	</style>")
- 	 .appendTo( "head" );
  });
+
+// намного быстрее CSS биндится для td  
+$( "<style> #Employees td.More { color: #0606CE } td.Less { color: #F30535 } 	</style>")
+ .appendTo( "head" );
+
